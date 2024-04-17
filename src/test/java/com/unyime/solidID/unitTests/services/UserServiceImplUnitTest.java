806cd3efcee858e5c080daef3564a5502e0b1da0
unyimeudoh <unyimeudemy20@gmail.com>
2024-04-17 17:23:48 +0100

    write unit test for userServiceImpl[COMPLETED]

--- src/test/java/com/unyime/solidID/unitTests/services/UserServiceImplUnitTest.java
+++ src/test/java/com/unyime/solidID/unitTests/services/UserServiceImplUnitTest.java
@@ -3,9 +3,15 @@ package com.unyime.solidID.unitTests.services;
 
 import com.unyime.solidID.TestDataUtility;
 import com.unyime.solidID.domain.AuthenticationResponse;
+import com.unyime.solidID.domain.entities.IdentityUsageRecordEntity;
+import com.unyime.solidID.domain.entities.OrganizationEntity;
 import com.unyime.solidID.domain.entities.UserEntity;
+import com.unyime.solidID.domain.entities.UserOrganizationEntity;
+import com.unyime.solidID.repository.IdentityUsageRecordRepository;
+import com.unyime.solidID.repository.UserOrganizationRepository;
 import com.unyime.solidID.repository.UserRepository;
 import com.unyime.solidID.services.impl.JwtServiceImpl;
+import com.unyime.solidID.services.impl.OrganizationServiceImpl;
 import com.unyime.solidID.services.impl.UserServiceImpl;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -17,6 +23,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import java.util.List;
 import java.util.Optional;
 
 import static org.mockito.Mockito.*;
@@ -34,6 +41,15 @@ public class UserServiceImplUnitTest {
     private PasswordEncoder passwordEncoder;
     @Mock
     private AuthenticationManager authenticationManager;
+
+    @Mock
+    private OrganizationServiceImpl organizationServiceImpl;
+
+    @Mock
+    private UserOrganizationRepository userOrganizationRepository;
+
+    @Mock
+    private IdentityUsageRecordRepository identityUsageRecordRepository;
     @InjectMocks
     private UserServiceImpl underTest;
 
@@ -118,5 +134,132 @@ public class UserServiceImplUnitTest {
         assertThat(result).isEqualTo(response);
     }
 
+    @Test
+    public void testThatCurrentUserCanGetHisProfileDetail(){
+        UserEntity userEntity = TestDataUtility.createTestUserEntity();
 
+        when(userRepository.findByEmail(userEntity.getEmail()))
+                .thenReturn(Optional.of(userEntity));
+
+        Optional<UserEntity> result = underTest.getProfile(userEntity.getEmail());
+        assertThat(result).isPresent();
+        assertThat(result.get()).isEqualTo(userEntity);
+    }
+
+    @Test
+    public void testThatGetProfileReturnsNullWhenUserDoesNotExist(){
+
+        when(userRepository.findByEmail(any(String.class)))
+                .thenReturn(Optional.empty());
+
+        Optional<UserEntity> result = underTest.getProfile("IncorrectEmail");
+        assertThat(result).isEmpty();
+    }
+
+
+    @Test
+    public void testThatUserCanAddOrgToAccAndAddedOrgIsReturned(){
+        OrganizationEntity organizationEntity = TestDataUtility.createTestOrgEntity();
+        UserOrganizationEntity userOrganizationEntity =
+                TestDataUtility.createTestUserOrgEntity();
+
+        when(organizationServiceImpl.getOrg(userOrganizationEntity.getOrgEmail()))
+                .thenReturn(Optional.of(organizationEntity));
+
+        when(userOrganizationRepository.save(userOrganizationEntity))
+                .thenReturn(userOrganizationEntity);
+
+        Optional<UserOrganizationEntity> result =
+                underTest.addOrganization(userOrganizationEntity);
+
+        assertThat(result).isPresent();
+        assertThat(result.get()).isEqualTo(userOrganizationEntity);
+    }
+
+    @Test
+    public void testThatAddOrgReturnsNullIfOrgIsNotRegistered(){
+        UserOrganizationEntity userOrganizationEntity =
+                TestDataUtility.createTestUserOrgEntity();
+
+        when(organizationServiceImpl.getOrg(any(String.class)))
+                .thenReturn(Optional.empty());
+
+        Optional<UserOrganizationEntity> result =
+                underTest.addOrganization(userOrganizationEntity);
+
+        assertThat(result).isEmpty();
+    }
+
+    @Test
+    public void testThatAlreadyRegisteredOrgWithUserReturnsTrue(){
+        UserOrganizationEntity userOrganizationEntity =
+                TestDataUtility.createTestUserOrgEntity();
+
+        when(userOrganizationRepository
+                .findByUserEmailAndOrgEmail(
+                        userOrganizationEntity.getStaffEmail(),
+                        userOrganizationEntity.getOrgEmail()
+                )
+        ).thenReturn(Optional.of(userOrganizationEntity));
+
+        Boolean result = underTest.checkIfOrgIsAlreadyRegisteredWithUser(
+                userOrganizationEntity.getStaffEmail(),
+                userOrganizationEntity.getOrgEmail()
+        );
+
+        assertThat(result).isTrue();
+    }
+
+    @Test
+    public void testThatNoneRegisteredOrgWithUserReturnsFalse(){
+
+        when(userOrganizationRepository
+                .findByUserEmailAndOrgEmail(
+                        any(String.class),
+                        any(String.class)
+                )
+        ).thenReturn(Optional.empty());
+
+        Boolean result = underTest.checkIfOrgIsAlreadyRegisteredWithUser(
+                "any wrong user email",
+                "any wrong org email"
+        );
+
+        assertThat(result).isFalse();
+    }
+
+
+    @Test
+    public void testThatUserCanGetListOfAllOrgsHeIsAMember(){
+        UserEntity userEntity = TestDataUtility.createTestUserEntity();
+        UserOrganizationEntity userOrganizationEntity = TestDataUtility.createTestUserOrgEntity();
+        List<UserOrganizationEntity> response = List.of(userOrganizationEntity);
+
+        when(userOrganizationRepository.findByStaffEmail(userEntity.getEmail()))
+                .thenReturn(List.of(userOrganizationEntity));
+
+        List<UserOrganizationEntity> result = underTest.getAllUserOrg(userEntity.getEmail());
+        assertThat(result).isNotEmpty();
+        assertThat(result).isEqualTo(response);
+    }
+
+
+    @Test
+    public void TestThatUserCanGetIdentityUsageRecord(){
+        UserEntity userEntity = TestDataUtility.createTestUserEntity();
+        IdentityUsageRecordEntity identityUsageRecordEntity =
+                TestDataUtility.createIdentityUsageRecordEntity();
+
+        List<IdentityUsageRecordEntity> response = List.of(identityUsageRecordEntity);
+
+        when(identityUsageRecordRepository.findByUserVerifiedEmail(userEntity.getEmail()))
+                .thenReturn(List.of(identityUsageRecordEntity));
+
+        List<IdentityUsageRecordEntity> result =
+                underTest.getIdentityUsageRecord(userEntity.getEmail());
+
+        assertThat(result).isNotEmpty();
+        assertThat(result).isEqualTo(response);
+
+    }
 }
