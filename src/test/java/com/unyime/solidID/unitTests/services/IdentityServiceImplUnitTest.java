f7ab0fffad45d56fe8e1648dfb32952d3afafddf
unyimeudoh <unyimeudemy20@gmail.com>
2024-04-21 20:16:10 +0100

    write unit test  for IdentityServiceImpl [COMPLETED]

--- src/test/java/com/unyime/solidID/unitTests/services/IdentityServiceImplUnitTest.java
+++ src/test/java/com/unyime/solidID/unitTests/services/IdentityServiceImplUnitTest.java
@@ -6,6 +6,7 @@ import com.unyime.solidID.domain.entities.*;
 import com.unyime.solidID.repository.IdentityRepository;
 import com.unyime.solidID.repository.IdentityUsageRecordRepository;
 import com.unyime.solidID.repository.UserOrganizationRepository;
+import com.unyime.solidID.repository.UserRepository;
 import com.unyime.solidID.services.impl.IdentityServiceImpl;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestFactory;
@@ -16,6 +17,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import static org.mockito.Mockito.*;
 import static org.assertj.core.api.Assertions.*;
 
+import java.time.LocalDateTime;
 import java.util.Optional;
 import java.util.Random;
 
@@ -30,6 +32,10 @@ public class IdentityServiceImplUnitTest {
 
     @Mock
     private IdentityUsageRecordRepository identityUsageRecordRepository;
+
+
+    @Mock
+    private UserRepository userRepository;
 
     @InjectMocks
     private IdentityServiceImpl underTest;
@@ -55,12 +61,43 @@ public class IdentityServiceImplUnitTest {
 
 
     @Test
-    public void testThatTokenCanBeVerifiedAndUserDetailsReturn(){
+    public void testThatTokenCanBeVerifiedAndUserDetailsReturnedWhenProfileHasBeenChosen(){
+        String key = "123456";
+        UserEntity verifiedUser = TestDataUtility.createTestUserEntity();
+        UserEntity userEntity = TestDataUtility.createTestUserEntity();
+        IdentityURLEntity identityURLEntity = TestDataUtility.createIdentityURLEntity();
+        identityURLEntity.setOrgEmail("Profile");
+        VerificationResponse response =  VerificationResponse.builder()
+                .firstName(verifiedUser.getFirstName())
+                .lastName(verifiedUser.getLastName())
+                .otherName(verifiedUser.getOtherName())
+                .stateOfOrigin(verifiedUser.getStateOfOrigin())
+                .email(verifiedUser.getEmail())
+                .image(verifiedUser.getImage())
+                .nationality(verifiedUser.getNationality())
+                .build();
+
+        when(identityRepository.findByKey(key))
+                .thenReturn(Optional.of(identityURLEntity));
+
+        when(userRepository.findByEmail(identityURLEntity.getEncodedEmail()))
+                .thenReturn(Optional.of(userEntity));
+
+        Optional<VerificationResponse> result = underTest
+                .verify(userEntity.getEmail(), key);
+
+        assertThat(result).isPresent();
+        assertThat(result.get()).isEqualTo(response);
+    }
+
+
+    @Test
+    public void testThatTokenCanBeVerifiedAndUserDetailsReturnedWhenOrgHasBeenChosen(){
         String key = "123456";
         UserOrganizationEntity userOrganizationEntity = TestDataUtility.createTestUserOrgEntity();
         UserEntity userEntity = TestDataUtility.createTestUserEntity();
         IdentityURLEntity identityURLEntity = TestDataUtility.createIdentityURLEntity();
-        identityURLEntity.setOrgEmail("Profile");
+        identityURLEntity.setOrgEmail("Not Profile");
         VerificationResponse response = VerificationResponse.builder()
                 .firstName(userOrganizationEntity.getStaffName())
                 .staffRole(userOrganizationEntity.getStaffRole())
@@ -74,13 +111,28 @@ public class IdentityServiceImplUnitTest {
                 identityURLEntity.getEncodedEmail(),
                 identityURLEntity.getOrgEmail()
         )).thenReturn(Optional.of(userOrganizationEntity));
-//        when(identityUsageRecordRepository.save())
 
         Optional<VerificationResponse> result = underTest
                 .verify(userEntity.getEmail(), key);
 
         assertThat(result).isPresent();
+        assertThat(result.get().getFirstName()).isEqualTo(response.getFirstName());
         assertThat(result.get()).isEqualTo(response);
+    }
 
+    @Test
+    public void testThatNullIsReturnedIfThereIsNoKeyToVerify(){
+        String key = "123456";
+        IdentityURLEntity identityURLEntity = TestDataUtility.createIdentityURLEntity();
+        identityURLEntity.setOrgEmail("Profile or Org");
+        UserEntity userEntity = TestDataUtility.createTestUserEntity();
+
+
+        when(identityRepository.findByKey(key)).thenReturn(Optional.empty());
+
+        Optional<VerificationResponse> result = underTest
+                .verify(userEntity.getEmail(), key);
+
+        assertThat(result).isEmpty();
     }
 }
